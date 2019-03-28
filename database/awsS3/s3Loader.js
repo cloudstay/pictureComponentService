74d00905835d257daf8bfb50d625ca98fe1e1b7c
Sutton <suco3693@gmail.com>
2019-03-28 12:49:58 -0700

    new branch to start creation of s3 bucket

--- database/awsS3/s3Loader.js
+++ database/awsS3/s3Loader.js
@@ -1,18 +1,33 @@
 var AWS = require("aws-sdk");
 var uuid = require("uuid");
 
-var bucketName= staycloud-Listing-bucketName;
+AWS.config.loadFromPath('../../config.json')
 
-var samplePhoto= "SampleURL";
+const bucketName= 'staycloudphotobucket';
 
-var photoBucketPromise= new AWS.S3({apiVersion : '2006-03-01'}).createBucket({Bucket : bucketName}).promise();
+var samplePhoto= "SampleURL2";
+
+var s3 = new AWS.S3();
+
+s3.client
 
 
-photoBucketPromise.then(
-  (data)=>{
-    var parameters={
-      Bucket: bucketName, 
-      Key: samplePhoto, 
-      Body: "ListingID"}
-  }
-)
+
+// var photoBucketPromise= new AWS.S3({apiVersion : '2006-03-01'}).createBucket({Bucket : bucketName}).promise();
+
+
+// photoBucketPromise.then(
+//   (data)=>{
+//     var parameters={
+//       Bucket: bucketName, 
+//       Key: samplePhoto, 
+//       Body: "ListingID"
+//     };
+//     var uploadPromise= new AWS.S3({apiVersion : '2006-03-01'}).putObject(parameters).promise();
+//     uploadPromise.then(
+//       (data)=>{
+//         console.log(`Successfully put data in ${bucketName}`)
+//     });
+// }).catch ((err) => {
+//     console.log("Err", err);
+// });

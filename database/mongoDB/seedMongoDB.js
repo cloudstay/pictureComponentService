bf1893cae747b6dcd55ad45c2d9c58699d996821
Sutton <suco3693@gmail.com>
2019-03-28 21:04:18 -0700

    Update to s3 and mongoDB files to seed both with random data. S3 photos must be manually loaded into s3 from s3seed loader

--- database/mongoDB/seedMongoDB.js
+++ database/mongoDB/seedMongoDB.js
@@ -1,37 +1,73 @@
 var mongooose = require('mongoose');
+const faker = require('faker');
+mongooose.connect('mongodb://127.0.0.1:27017/photoCarousel');
 
-mongooose.connect("mongodb://127.0.0.1:27017/photoCarousel");
 
-
-var database= mongooose.connection;
+var database = mongooose.connection;
 
 database.once('open', ()=>{
-  console.log("Database is connected");
-})
+  console.log('Database is connected');
+});
 
 var photoCarouselSchema = new mongooose.Schema({
-  photoID : {
-    type : String ,
-    required : true,
-    maxlength : 7,
-    minlength : 7
+  photoID: {
+    type: String,
+    required: true,
+    maxlength: 7,
+    minlength: 7
   }, 
-  roomID : {
-    type : String,
-    required : true,
-    maxlength : 3,
-    minlength : 3
+  ListingID: {
+    type: String,
+    required: true,
+    maxlength: 11,
+    minlength: 11
   },
-  photoURL : {
-    type : String,
-    required : true
+  photoURL: {
+    type: String,
+    required: true
   },
-  photoDescription : {
-    type : String,
-    required : true
+  photoDescription: {
+    type: String,
+    required: true
   }
 
-})
+});
 
+var mokeDocument = function(ListingID, photoIndex) {
+  //roomID is the value of the room in my s3 database
+  //100-119: interior photos
+  //120-139: Kitchen photos
+  //140-159: bathroom photos
+  //160-179: bedroom photos
+  //180-200: house photos
+  var roomID = Math.floor(Math.random() * 100) + 100;
+  return {
+    photoID: `${ListingID}_${photoIndex}`,
+    ListingID: `Listing-${ListingID}`,
+    photoURL: `https://s3-us-west-1.amazonaws.com/staycloudphotobucket/${roomID}-roomImage.jpg`,
+    photoDescription: faker.lorem.sentences()
+  };
+};
 
-var makeCollection= function(){};
+var makeCollection = function(ListingID) {
+  var ListingCollection = mongooose.model(`listing${ListingID}photo`, photoCarouselSchema);
+  var seedDocuments = [];
+  var photos = Math.floor(Math.random() * 20) + 100;
+  for (var photoID = 100; photoID < photos; photoID++) {
+    seedDocuments.push(mokeDocument(ListingID, photoID));
+  }
+
+  ListingCollection.insertMany(seedDocuments, (err)=>{
+    if (err) {
+      console.log(err);
+    } else {
+      if (ListingID === 105) {
+        mongooose.connection.close();
+      }
+    }
+  });
+};
+
+for (var i = 100; i < 200; i++ ) {
+  makeCollection(i);
+}
--- database/s3/s3seed.js
+++ database/s3/s3seed.js
@@ -1,26 +1,26 @@
-const request = require("request");
-const fs= require("fs");
-const Promise = require("bluebird");
+const request = require('request');
+const fs = require('fs');
+const Promise = require('bluebird');
 
 Promise.promisifyAll(request);
 Promise.promisifyAll(fs);
 
-var keyword=`interior`;
+var keyword = 'interior';
 //create a random photo from unsplash based on keyword given
-var getNewunsplash= function(imageCount, keyword){
+var getNewunsplash = function(imageCount, keyword) {
   request(`https://source.unsplash.com/random/800x600/?${keyword}`)
     .pipe(fs.createWriteStream(`./media/${imageCount}-roomImage.jpg`));
-}
+};
 
-for(var i =100; i< 201; i++){
-  if(i >= 120 && i < 140){
-    keyword='kitchen';
-  } else if ( i >= 140 && i < 160){
-    keyword='bathroom';
-  } else if (i >= 160 && i < 180){
-    keyword=`house`;
+for (var i = 100; i < 201; i++) {
+  if (i >= 120 && i < 140) {
+    keyword = 'kitchen';
+  } else if ( i >= 140 && i < 160) {
+    keyword = 'bathroom';
+  } else if (i >= 160 && i < 180) {
+    keyword = 'house';
   } else {
-    keyword=`bedroom`;
+    keyword = 'bedroom';
   }
   getNewunsplash(i, keyword);
 }

252f1639d413d42b61980d165b3da7d713d7a91b
Sutton <suco3693@gmail.com>
2019-03-29 19:14:16 -0700

    Update to express server to accept ListingID in query

--- server/expressServer.js
+++ server/expressServer.js
@@ -8,7 +8,7 @@ const app = express();
 const port = 3006;
 
 // app set at localhost:3006/listing/
-app.use('/listing', express.static('public'));
+app.use('/', express.static('public'));
 
 // use of body parser
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -17,8 +17,8 @@ app.use(bodyParser.json());
 
 // get request based on room number
 
-app.get('/listing/:listingID', (req, res) => {
-  getDB.getDBcollection(req.url.slice(9, 12), (err, photoDocuments) => {
+app.get('/api/rooms', (req, res) => {
+  getDB.getDBcollection(req.query.id, (err, photoDocuments) => {
     if (err) {
       res.status(404);
       res.end();

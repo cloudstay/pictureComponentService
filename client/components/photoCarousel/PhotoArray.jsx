fb52fc8af90d47300c1c74f28dde3df1c8fd8576
Sutton <suco3693@gmail.com>
2019-04-05 15:24:54 -0700

    Update to photo Array to move with click

--- client/components/photoCarousel/PhotoArray.jsx
+++ client/components/photoCarousel/PhotoArray.jsx
@@ -12,8 +12,18 @@ var PhotoArray = (props) => {
       <input onClick={()=>props.changePhotoCarMainPhoto(props.photos.indexOf(photo))} className="photoArrayItem" type="image" style={props.currentPhotoIndex === props.photos.indexOf(photo) ? activePhotoStyle:{}}  src={photo.photoURL} alt={photo.photoDescription} />
     </li>
   )
+  var translateX= {
+    transform: `translateX(0px)`
+  }
+  if(props.currentPhotoIndex < 3){
+    translateX.transform = `translateX(0px)`
+  } else if (props.currentPhotoIndex >= 3 && props.currentPhotoIndex <= props.photos.length-3){
+    translateX.transform =`translateX(${-110*(props.currentPhotoIndex-2)}px)`
+  } else if(props.currentPhotoIndex > props.photos.length-3) {
+    translateX.transform =`translateX(${-110*(props.photos.length-5)}px)`
+  }
   return (
-    <ul className="photoArrayContainer pcUnderPhoto ">
+    <ul className="photoArrayContainer pcUnderPhoto " style={translateX}>
       {photoArray}
     </ul>
   )

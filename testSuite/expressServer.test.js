6b25ee3b171a5b2f4c771e3163705c39bd38c9b3
Sutton <suco3693@gmail.com>
2019-04-15 20:17:26 -0700

    Update to expressServer test suite with new api routes

--- testSuite/expressServer.test.js
+++ testSuite/expressServer.test.js
@@ -2,27 +2,26 @@ const request = require('request');
 
 describe('Testing GET pathway', () => {
   test('Except get to return to be a string', (done) => {
-    request('http://127.0.0.1:3006/rooms/api?id=104', (err, res, body) => {
-      console.log(typeof body);
+    request('http://127.0.0.1:3006/api/rooms/104/photos', (err, res, body) => {
       expect(typeof body).toBe('string');
       done();
     });
   });
   test('Except get to return with 200 ', (done) => {
-    request('http://127.0.0.1:3006/rooms/api?id=104', (err, res, body) => {
+    request('http://127.0.0.1:3006/api/rooms/104/photos', (err, res, body) => {
       expect(res.statusCode).toBe(200);
       done();
     });
   });
   test('Except get to return to be parsable ', (done) => {
-    request('http://127.0.0.1:3006/rooms/api?id=104', (err, res, body) => {
+    request('http://127.0.0.1:3006/api/rooms/104/photos', (err, res, body) => {
       const pictureArray = JSON.parse(body);
       expect(typeof pictureArray).toBe('object');
       done();
     });
   });
   test('Except get to parsed object to have correct ids ', (done) => {
-    request('http://127.0.0.1:3006/rooms/api?id=104', (err, res, body) => {
+    request('http://127.0.0.1:3006/api/rooms/104/photos', (err, res, body) => {
       const pictureArray = JSON.parse(body);
       expect(pictureArray[0].photoID).toBe('104_100');
       expect(pictureArray[0].ListingID).toBe('Listing-104');
